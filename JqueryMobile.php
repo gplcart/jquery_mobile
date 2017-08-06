3a0748709f486a3ea9a8db700b39eb6c681103e6
GplCart <gplcart.software@gmail.com>
2017-08-06 16:11:14 +0300

    Add code base

---
+++ JqueryMobile.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * @package Jquery Mobile
+ * @author Iurii Makukh <gplcart.software@gmail.com>
+ * @copyright Copyright (c) 2017, Iurii Makukh <gplcart.software@gmail.com>
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html GPL-3.0+
+ */
+
+namespace gplcart\modules\jquery_mobile;
+
+use gplcart\core\Module;
+
+/**
+ * Main class for Jquery Mobile module
+ */
+class JqueryMobile extends Module
+{
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Implements hook "library.list"
+     * @param array $libraries
+     */
+    public function hookLibraryList(array &$libraries)
+    {
+        $libraries['jquery_mobile'] = array(
+            'name' => 'Jquery Mobile',
+            'description' => 'A HTML5-based user interface system designed to make responsive web sites and apps that are accessible on all smartphone, tablet and desktop devices',
+            'type' => 'asset',
+            'module' => 'jquery_mobile',
+            'url' => 'jquerymobile.com',
+            'download' => 'http://jquerymobile.com/resources/download/jquery.mobile-1.4.5.zip',
+            'version_source' => array(
+                'file' => 'vendor/jquery/jquery-mobile/jquery.mobile-1.4.5.min.js',
+                'pattern' => '/jQuery Mobile (\\d+\\.+\\d+\\.+\\d+)/',
+                'lines' => 2
+            ),
+            'files' => array(
+                'vendor/jquery/jquery-mobile/jquery.mobile-1.4.5.min.js',
+                'vendor/jquery/jquery-mobile/jquery.mobile-1.4.5.min.css',
+            ),
+        );
+    }
+
+    /**
+     * Implements hook "module.install.before"
+     */
+    public function hookModuleInstallBefore(&$result)
+    {
+        foreach (array('jquery' => '1.8.0', 'jquery_ui' => '1.10') as $id => $version) {
+            $library = $this->getLibrary()->get($id);
+            if (version_compare($library['version'], $version, '<')) {
+                $result = $this->getLanguage()->text('Jquery Mobile requires Jquery >= 1.8.0 and Jquery UI >= 1.10');
+                break;
+            }
+        }
+    }
+
+    /**
+     * Implements hook "module.enable.after"
+     */
+    public function hookModuleEnableAfter()
+    {
+        $this->getLibrary()->clearCache();
+    }
+
+    /**
+     * Implements hook "module.disable.after"
+     */
+    public function hookModuleDisableAfter()
+    {
+        $this->getLibrary()->clearCache();
+    }
+
+    /**
+     * Implements hook "module.install.after"
+     */
+    public function hookModuleInstallAfter()
+    {
+        $this->getLibrary()->clearCache();
+    }
+
+    /**
+     * Implements hook "module.uninstall.after"
+     */
+    public function hookModuleUninstallAfter()
+    {
+        $this->getLibrary()->clearCache();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+[![Build Status](https://scrutinizer-ci.com/g/gplcart/jquery_mobile/badges/build.png?b=master)](https://scrutinizer-ci.com/g/gplcart/jquery_mobile/build-status/master)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/gplcart/jquery_mobile/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/gplcart/jquery_mobile/?branch=master)
+
+Jquery Mobile is a [GPL Cart](https://github.com/gplcart/gplcart) module that adds [Jquery Mobile](http://jquerymobile.com) library which can be used by other modules. Do not install unless your modules require it.
+
+
+**Installation**
+
+1. Download and extract to `system/modules` manually or using composer `composer require gplcart/jquery_mobile`. IMPORTANT: If you downloaded the module manually, be sure that the name of extracted module folder doesn't contain a branch/version suffix, e.g `-master`. Rename if needed.
+2. Go to `admin/module/list` end enable the module
