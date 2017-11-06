31a2a44ea8db7614aff0a333655b20c094fbd464
GplCart <gplcart.software@gmail.com>
2017-11-06 17:48:52 +0200

    Core API changes

--- JqueryMobile.php
+++ JqueryMobile.php
@@ -9,7 +9,8 @@
 
 namespace gplcart\modules\jquery_mobile;
 
-use gplcart\core\Module;
+use gplcart\core\Module,
+    gplcart\core\Config;
 
 /**
  * Main class for Jquery Mobile module
@@ -18,11 +19,11 @@ class JqueryMobile extends Module
 {
 
     /**
-     * Constructor
+     * @param Config $config
      */
-    public function __construct()
+    public function __construct(Config $config)
     {
-        parent::__construct();
+        parent::__construct($config);
     }
 
     /**
